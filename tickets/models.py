3b3ddd58213c2c71717ccdd5c758264a337ab98e
AbdoEldgwy <abdo.eldgwy1@gmail.com>
2025-04-23 21:42:07 +0200

    add models classes

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Guest -- Moive -- reservations 
+class Guest(models.Model):
+    Name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=10)
+
+class Movie(models.Model):
+    Hall = models.CharField(max_length=20)
+    Movie_Name = models.CharField(max_length=50)
+    Show_Time = models.DateTimeField()
+    Price = models.IntegerField()
+
+class Reservation(models.Model):
+    Guest = models.ForeignKey(Guest, on_delete=models.CASCADE, related_name='reservations')
+    Movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reservations')
+    Seat_Number = models.CharField(max_length=10)
